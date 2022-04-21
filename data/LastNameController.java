367e27698ddf66a1df8f09407e1c5559596c1993
NSAHITHI007 <90211550+NSAHITHI007@users.noreply.github.com>
2022-04-21 14:05:23 +0530

    Create LastNameController.java

---
+++ data/LastNameController.java
@@ -0,0 +1,13 @@
+package com.oms.controller.data;
+
+
+
+import com.oms.controller.AbstractController;
+import com.oms.model.data.LastName;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/last-name")
+public class LastNameController extends AbstractController<LastName> {
+}
